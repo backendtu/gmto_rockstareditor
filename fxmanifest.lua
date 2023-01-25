f644df68b52b34793a0c6f3473fed7886c9d9f96
gmtotu <90359341+backendtu@users.noreply.github.com>
2023-01-25 16:10:19 -0500

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+game 'gta5'
+fx_version 'cerulean'
+
+author 'gmtotu#9322'
+description 'https://discord.gg/risingdev'
+
+lua54 'yes'
+version '1.0.0'
+
+client_scripts {
+    'client/main.lua'
+}
+shared_scripts {
+    '@ox_lib/init.lua'
+}
